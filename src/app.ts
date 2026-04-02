ce59f25f9386df619699d5daf12b6a40797b405e
karanvir singh <ksingh34@academic.rrc.ca>
2026-04-02 18:48:37 -0500

    fix: fixed the student routes imports.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
+import studentRoutes from "./api/v1/routes/studentRoutes";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/v1/health", (_req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "API is healthy",
-  });
+  res.json({ status: "ok" });
 });
+
+app.use("/api/v1/students", studentRoutes);
 
 export default app;
