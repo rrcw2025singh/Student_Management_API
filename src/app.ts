604db2370c320996797fc64fb61f9111983551b0
karanvir singh <ksingh34@academic.rrc.ca>
2026-04-02 13:13:19 -0500

    fix: Fixed the imports in express file.

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/v1/health", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API is healthy",
+  });
+});
+
+export default app;
