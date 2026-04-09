1e66c1b76142bc0e4af7adb281e2349a02a78728
karanvir singh <ksingh34@academic.rrc.ca>
2026-04-09 09:02:00 -0500

    feat: register the swagger UI endpoint in the application.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import studentRoutes from "./api/v1/routes/studentRoutes";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/v1/health", healthRoutes);
 app.use("/api/v1/students", studentRoutes);
