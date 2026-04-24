b57a30f1845b9845c6cddbd32adb59060361e643
karanvir singh <ksingh34@academic.rrc.ca>
2026-04-24 00:25:07 -0500

    chore: load server port from environment.

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ import app from "./app";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
